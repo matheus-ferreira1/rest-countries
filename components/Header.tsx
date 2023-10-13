7e613cba488ae51fc0a1b2d59b2379b7a7272dc3
matheus-ferreira1 <matheus.fer33@gmail.com>
2023-10-12 21:33:30 -0300

    fix: header brand is now a link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,17 @@
 import { FC } from "react";
 import { Globe2 } from "lucide-react";
 import { ToggleTheme } from "./ToggleTheme";
+import Link from "next/link";
 
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = ({}) => {
   return (
     <header className="border-b flex justify-between items-center py-4">
-      <div className="flex items-center gap-2">
+      <Link href="/" className="flex items-center gap-2">
         <Globe2 />
         <h1 className="font-bold text-xl">Countries</h1>
-      </div>
+      </Link>
       <ToggleTheme />
     </header>
   );
